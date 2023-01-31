1c567d809c198abcd52d1202b488e0aefb665dd9
Tavish Noel <tavish.noel@me.com>
2023-01-31 11:03:07 -0600

    first commit

---
+++ react-admin/src/theme.js
@@ -0,0 +1,60 @@
+import { createContext, useState, useMemo } from "react";
+import { createTheme } from "@mui/system/styles";
+
+//color design
+//use extention "Color Highlight" to better see the different shades I am working with 
+indigo: {
+    100: "#e0e0e0",
+    200: "#c2c2c2",
+    300: "#a3a3a3",
+    400: "#858585",
+    500: "#666666",
+    600: "#525252",
+    700: "#3d3d3d",
+    800: "#292929",
+    900: "#141414"
+},
+black: {
+    100: "#d0d1d5",
+    200: "#a1a4ab",
+    300: "#727681",
+    400: "#434957",
+    500: "#141b2d",
+    600: "#101624",
+    700: "#0c101b",
+    800: "#080b12",
+    900: "#040509"
+},
+green: {
+    100: "#dbf5ee",
+    200: "#b7ebde",
+    300: "#94e2cd",
+    400: "#70d8bd",
+    500: "#4cceac",
+    600: "#3da58a",
+    700: "#2e7c67",
+    800: "#1e5245",
+    900: "#0f2922"
+},
+red: {
+    100: "#f8dcde",
+    200: "#f1b9bd",
+    300: "#e9959c",
+    400: "#e2727b",
+    500: "#db4f5a",
+    600: "#af3f48",
+    700: "#832f36",
+    800: "#582024",
+    900: "#2c1012"
+},
+indigo: {
+    100: "#e1e2fe",
+    200: "#c3c6fd",
+    300: "#a4a9fc",
+    400: "#868dfb",
+    500: "#6870fa",
+    600: "#535ac8",
+    700: "#3e4396",
+    800: "#2a2d64",
+    900: "#151632"
+},
